e4df34ef1a6443854c3b6156151f0d6fd1935828
Aziz Alfauzi <azizalfauzi@student.telkomuniversity.ac.id>
2023-09-18 06:32:21 +0700

    MYSQL - Alias

--- sql_product.session.sql
+++ sql_product.session.sql
@@ -6,85 +6,88 @@ create table product(
     quantity int unsigned not null default 0,
     created_at timestamp not null default CURRENT_TIMESTAMP
 ) engine = InnoDb;
-
 show tables;
-
 show create table product;
-
 DESCRIBE product;
-
 -- Perintah untuk insert ke dalam table product tanpa deskripsi
-INSERT into product (id,name,price,quantity)
-values ('P001','Buku python',30000,100);
+INSERT into product (id, name, price, quantity)
+values ('P001', 'Buku python', 30000, 100);
 -- Perintah untuk insert ke dalam table product dengan  deskripsi
-INSERT into product (id,name,deskripsi,price,quantity)
-values ('P002','Buku dart','Belajar bahasa pemrograman dart',25000,50);
-
+INSERT into product (id, name, deskripsi, price, quantity)
+values (
+        'P002',
+        'Buku dart',
+        'Belajar bahasa pemrograman dart',
+        25000,
+        50
+    );
 -- Perintah untuk insert ke dalam table product dengan banyak data
-INSERT into product (id,name,price,quantity)
-values ('P007','Buku dart',76000,200),
-('P008','Buku xss',30000,120),
-('P009','Buku android',90000,110),
-('P0010','Buku ios',80000,120),
-('P0011','Buku .net',85000,120),
-('P0012','Buku spring boot',95000,110);
-
+INSERT into product (id, name, price, quantity)
+values ('P007', 'Buku dart', 76000, 200),
+    ('P008', 'Buku xss', 30000, 120),
+    ('P009', 'Buku android', 90000, 110),
+    ('P0010', 'Buku ios', 80000, 120),
+    ('P0011', 'Buku .net', 85000, 120),
+    ('P0012', 'Buku spring boot', 95000, 110);
 -- Perintah select seluruh barang
-SELECT * FROM product;
-
-
+SELECT *
+FROM product;
 -- Perintah select dengan menyebut nmanya
-SELECT id,name,quantity FROM product;
-
+SELECT id,
+    name,
+    quantity
+FROM product;
 -- Perintah untuk menambahkan primary key
-
-ALTER TABLE product 
+ALTER TABLE product
 ADD primary key (id);
-
-INSERT into product (id,name,price,quantity)
-values ('P001','Buku python',30000,100);
-
+INSERT into product (id, name, price, quantity)
+values ('P001', 'Buku python', 30000, 100);
 -- Perintah where clause
-
-SELECT * FROM product WHERE quantity = 120;
-
-SELECT * FROM product WHERE price = 25000;
-
-SELECT * FROM product WHERE id = 'P001';
-
-SELECT * FROM product WHERE name = 'Buku dart';
-
-
-
+SELECT *
+FROM product
+WHERE quantity = 120;
+SELECT *
+FROM product
+WHERE price = 25000;
+SELECT *
+FROM product
+WHERE id = 'P001';
+SELECT *
+FROM product
+WHERE name = 'Buku dart';
 DESCRIBE product;
 -- Perintah menambahkan kolom baru
 ALTER TABLE product
-ADD column category ENUM ('Back End','Front End','Lain-lain')
+ADD column category ENUM ('Back End', 'Front End', 'Lain-lain')
 AFTER name;
-
-SELECT * from product;
-
+SELECT *
+from product;
 -- Perintah update data single
 UPDATE product
-SET category ='Back End'
-WHERE id='P001';
-
+SET category = 'Back End'
+WHERE id = 'P001';
 --Perintah update data multiple
-
 UPDATE product
-SET category ='Front End',
-deskripsi='Buku untuk front end'
-WHERE id='P002'
-
-
--- Perintah update dengan value kolom 
-
+SET category = 'Front End',
+    deskripsi = 'Buku untuk front end'
+WHERE id = 'P002' -- Perintah update dengan value kolom 
 UPDATE product
 SET price = price + 5000
 WHERE id = 'P008';
-
-
 -- Perintah delete data pada table
-DELETE FROM 
-product WHERE id='P0012';
-
+DELETE FROM product
+WHERE id = 'P0012';
+-- Perintah ALIAS pada kolom
+SELECT id AS 'Kode',
+    name AS 'Nama',
+    category AS 'Kategori',
+    price AS 'Harga',
+    quantity AS 'Jumlah'
+FROM product;
+-- Perintah ALIAS pada table
+SELECT p.id AS 'Kode',
+    p.name AS 'Nama',
+    p.category AS 'Kategori',
+    p.price AS 'Harga',
+    p.quantity AS 'Jumlah'
+FROM product AS p;
