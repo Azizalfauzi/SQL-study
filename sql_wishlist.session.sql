7f1ca6f36a3f5d6121c40817e1e74c050727e4d9
Aziz Alfauzi <azizalfauzi@student.telkomuniversity.ac.id>
2023-09-26 05:22:19 +0700

    MYSQL - Join

--- sql_wishlist.session.sql
+++ sql_wishlist.session.sql
@@ -19,3 +19,31 @@ SELECT *
 FROM wishlist;
 DELETE FROM product
 where id = 'P013';
+--Perintah JOIN 
+SELECT *
+FROM wishlist
+    JOIN product ON(wishlist.id_product = product.id);
+SELECT w.id as id_wishlist,
+    p.id as id_product,
+    p.name as product_name,
+    w.description as description
+FROM wishlist as w
+    JOIN product as p ON(w.id_product = p.id);
+DESCRIBE wishlist;
+ALTER TABLE wishlist
+ADD COLUMN id_customer INT;
+ALTER TABLE wishlist
+ADD CONSTRAINT fk_wishlist_customer FOREIGN KEY (id_customer) REFERENCES customer(id);
+SELECT *
+FROM customer;
+UPDATE wishlist
+SET id_customer = 1
+WHERE id = 1;
+-- Perintah join beberapa table
+SELECT customer.email,
+    product.id,
+    product.name,
+    wishlist.description
+FROM wishlist
+    JOIN product ON(product.id = wishlist.id_product)
+    JOIN customer ON (customer.id = wishlist.id_customer);
